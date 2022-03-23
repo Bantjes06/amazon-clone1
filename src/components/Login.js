08e3bce30445d0078c4ea523a6d17d4908f82c7c
Johan Bantjes <82716250+Bantjes06@users.noreply.github.com>
2022-03-23 19:25:01 +0200

    Updating of login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <>
-      <h1>Sign-In</h1>
-    </>
-  )
-}
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-export default Login
+  const signIn = (e) => {
+    e.preventDefault();
+    const ennteredEmail = emailRef.current.value;
+    const ennteredPassword = passwordRef.current.value;
+    console.log("Email: ", ennteredEmail + " Password", ennteredPassword);
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="Amazon Logo"
+          className="login_logo"
+        />
+      </Link>
+      <div className="login_container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" ref={emailRef} />
+          <h5>Password</h5>
+          <input type="password" ref={passwordRef} />
+          <button type="submit" className="login_signInButton" onClick={signIn}>
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login_registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
